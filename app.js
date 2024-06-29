119eca6cbc93ab3341cf2f13c699bee987b3cdec
josephine19001 <gyameraj@gmail.com>
2024-06-29 23:25:02 +0300

    Fix errors

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
+
+app.get('/health', (req, res) => {
+  res.send('ok');
+});
 
 app.get('/version', (req, res) => {
   res.send('hello world!');
